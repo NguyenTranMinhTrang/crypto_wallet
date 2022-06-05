f0e4cd032adab85c33512a4dac59bffaa7010a21
Minh Trang <minhtrang.9096@gmail.com>
2022-06-06 00:06:10 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from 'expo-font';
+import Tabs from './app/navigation/tabs';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+
+  const [loaded] = useFonts({
+    "Roboto-Black": require('./app/assets/fonts/Roboto-Black.ttf'),
+    "Roboto-Bold": require('./app/assets/fonts/Roboto-Bold.ttf'),
+    "Roboto-Regular": require('./app/assets/fonts/Roboto-Regular.ttf'),
+    "Roboto-Light": require('./app/assets/fonts/Roboto-Light.ttf'),
+  })
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+        initialRouteName={'MainLayout'}
+      >
+        <Stack.Screen
+          name="MainLayout"
+          component={Tabs}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => {
+  return <App />;
+};
---
+++ app/components/TabIcon.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {
+    View,
+    Text
+} from "react-native";
+
+const TabIcon = () => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+export default TabIcon;
